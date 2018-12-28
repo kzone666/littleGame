428423c46eb8b91354bc3095f0d6ef14b3e0bb74
cbm-lraulier <kzone666@gmail.com>
2018-12-28 23:14:21 +0100

    init implementation

--- src/main/scala/org/kzone666/fr/Game.scala
+++ src/main/scala/org/kzone666/fr/Game.scala
@@ -1,7 +1,22 @@
 package org.kzone666.fr
 
 import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.glfw.GLFW._
+import org.lwjgl.opengl.GL11._
 
 class Game {
   val errorCallback: GLFWErrorCallback = GLFWErrorCallback.createPrint(System.err)
+  def window  = glfwCreateWindow(640, 480, "Simple example", 0l,0l)
+    throw new RuntimeException("Failed to create the GLFW window")
+    /*=> glfwTerminate*/
+
+  def initGame() = {
+    // Setup an error callback. The default implementation
+    // will print the error message in System.err.
+    GLFWErrorCallback.createPrint(System.err).set
+    // Initialize GLFW. Most GLFW functions will not work before doing this.
+    if (!glfwInit) throw new IllegalStateException("Unable to initialize GLFW")
+  }
+  def startGame() = ???
+  def stopGame() = ???
 }
--- src/main/scala/org/kzone666/fr/main.scala
+++ src/main/scala/org/kzone666/fr/main.scala
@@ -1,10 +1,6 @@
 package org.kzone666.fr
 
-import org.kzone666.fr
-import org.kzone666.fr._
-
 class main extends  App {
-  def game  = new Game
-
-  game.errorCallback.free()
+  val game  = new Game
+  game.initGame
 }
