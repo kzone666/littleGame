80b4011c304a7a48e78339ebdee2a872db96166c
cbm-lraulier <kzone666@gmail.com>
2018-12-31 08:58:03 +0100

    defintion of an engine game => trait (interface) engine IGameLogic

---
+++ src/main/scala/org/kzone666/fr/engine/IGameLogic.scala
@@ -0,0 +1,14 @@
+package org.kzone666.fr.engine
+
+trait IGameLogic {
+  /*
+  definition du moteur de jeu
+  handle user input
+  update game state
+  render the screen
+   */
+  def handleUserInput = ???
+  def updateGameState = ???
+  def renderToTheScree = ???
+}
+
